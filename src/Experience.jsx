4e5eb6978f0137bff8f0d82fbd36fe86e2c594b2
Ivan <4esoquevedoi@gmail.com>
2024-02-04 23:31:00 +0100

    feat: Shadows

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -2,6 +2,12 @@ import React, { useRef } from "react";
 // import CustomObject from "./CustomObject";
 import {
   OrbitControls,
+  useHelper,
+  BakeShadows,
+  SoftShadows,
+  AccumulativeShadows,
+  RandomizedLight,
+  ContactShadows,
   // TransformControls,
   // PivotControls,
   // Html,
@@ -10,8 +16,11 @@ import {
 } from "@react-three/drei";
 import { useControls } from "leva";
 import { Perf } from "r3f-perf";
+import { useFrame } from "@react-three/fiber";
+import { DirectionalLightHelper } from "three";
 
 const Experience = () => {
+  const directionLightRef = useRef(null);
   const cubeRef = useRef(null);
   const { visibility } = useControls("Perfomance", { visibility: true });
   const cubeSphereRef = useRef(null);
@@ -21,16 +30,76 @@ const Experience = () => {
     color: "#ff0000",
   });
 
+  const contactShadowControls = useControls("Contact Shadows", {
+    color: "#316d39",
+    opacity: {
+      value: 0.3,
+      min: 0,
+      max: 1,
+      step: 0.01,
+    },
+    blur: {
+      value: 3,
+      min: 0,
+      max: 10,
+      step: 0.01,
+    },
+  });
+
+  useHelper(directionLightRef, DirectionalLightHelper, 1);
+
+  useFrame((state, delta) => {
+    // const time = state.clock.elapsedTime;
+    // cubeRef.current.position.x = 2 + Math.sin(time);
+    cubeRef.current.rotation.y += delta * 0.5;
+  });
+
   return (
     <>
       {visibility && <Perf position="top-left" />}
+      {/* <BakeShadows /> */}
+      {/* <SoftShadows size={5} samples={10} focus={0} /> */}
+      {/* <AccumulativeShadows
+        position={[0, -0.99, 0]}
+        color="#316d39"
+        opacity={0.8}
+        frames={100}
+        temporal
+      >
+        <RandomizedLight
+          amount={8}
+          radius={1}
+          ambient={0.5}
+          intensity={3}
+          position={[1, 2, 3]}
+          bias={0.001}
+        />
+      </AccumulativeShadows> */}
+      <ContactShadows
+        position={[0, -0.99, 0]}
+        color={contactShadowControls.color}
+        opacity={contactShadowControls.opacity}
+        blur={contactShadowControls.blur}
+        frames={1} //bake
+      />
       <OrbitControls makeDefault />
-      <directionalLight position={[1, 2, 3]} intensity={4.5} />
+      <directionalLight
+        ref={directionLightRef}
+        castShadow
+        position={[1, 2, 3]}
+        intensity={4.5}
+        shadow-mapSize={[1024, 1024]}
+      />
       <ambientLight intensity={1.5} />
       {/* <CustomObject /> */}
       <group ref={cubeSphereRef}>
         {/* <PivotControls anchor={[0, 0, 0]} depthTest={false}> */}
-        <mesh ref={sphereRef} position={[position.x, position.y, 0]}>
+        <mesh
+          ref={sphereRef}
+          castShadow
+          receiveShadow
+          position={[position.x, position.y, 0]}
+        >
           <sphereGeometry />
           <meshStandardMaterial color={color} />
           {/* <Html
@@ -46,6 +115,8 @@ const Experience = () => {
         {/* </PivotControls> */}
         <mesh
           ref={cubeRef}
+          castShadow
+          receiveShadow
           position-x={2}
           rotation-y={Math.PI * 0.25}
           scale={1.5}
--- src/index.jsx
+++ src/index.jsx
@@ -6,12 +6,21 @@ import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
+// const onCanvasCreate = ({ gl, scene }) => {
+//   // gl.setClearColor("red", 1) color + alpha
+//   // scene.background = new THREE.Color("#ff0000")
+// };
+
+// onCreated={onCanvasCreate}
 
 const App = () => {
   return (
     <StrictMode>
       <Leva collapsed />
-      <Canvas>
+      <Canvas
+      // shadows
+      >
+        <color args={["ivory"]} attach="background" />
         <Experience />
       </Canvas>
     </StrictMode>
