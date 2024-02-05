eef3ab246248e1833f084797b0c6dab1add2b45d
Ivan <4esoquevedoi@gmail.com>
2024-02-05 23:15:28 +0100

    feat: Environment maps test

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -9,6 +9,8 @@ import {
   RandomizedLight,
   ContactShadows,
   Sky,
+  Environment,
+  Lightformer,
   // TransformControls,
   // PivotControls,
   // Html,
@@ -27,7 +29,7 @@ const Experience = () => {
   const cubeSphereRef = useRef(null);
   const sphereRef = useRef(null);
   const { position, color } = useControls("Sphere", {
-    position: { value: { x: -2, y: 0 }, step: 0.01, joystick: "invertY" },
+    position: { value: { x: -2, y: 1 }, step: 0.01, joystick: "invertY" },
     color: "#ff0000",
   });
 
@@ -53,6 +55,34 @@ const Experience = () => {
     },
   });
 
+  const { envMapIntensity, envMapHeight, envMapScale, envMapRadius } =
+    useControls("Environment map", {
+      envMapIntensity: {
+        value: 7,
+        min: 0,
+        max: 12,
+        step: 1,
+      },
+      envMapHeight: {
+        value: 7,
+        min: 0,
+        max: 100,
+        step: 1,
+      },
+      envMapScale: {
+        value: 100,
+        min: 10,
+        max: 1000,
+        step: 1,
+      },
+      envMapRadius: {
+        value: 28,
+        min: 10,
+        max: 1000,
+        step: 1,
+      },
+    });
+
   useHelper(directionLightRef, DirectionalLightHelper, 1);
 
   useFrame((state, delta) => {
@@ -64,6 +94,33 @@ const Experience = () => {
   return (
     <>
       {visibility && <Perf position="top-left" />}
+      <Environment
+        // background
+        // files={[
+        //   "./environmentMaps/2/px.jpg",
+        //   "./environmentMaps/2/nx.jpg",
+        //   "./environmentMaps/2/py.jpg",
+        //   "./environmentMaps/2/ny.jpg",
+        //   "./environmentMaps/2/pz.jpg",
+        //   "./environmentMaps/2/nz.jpg",
+        // ]}
+        // files="./environmentMaps/the_sky_is_on_fire_2k.hdr"
+        preset="sunset"
+        // resolution={32}
+        ground={{
+          height: envMapHeight,
+          radius: envMapRadius,
+          scale: envMapScale,
+        }}
+      >
+        {/* <color args={["black"]} attach="background" />
+        <Lightformer position-z={-5} scale={10} color="red" intensity={10} /> */}
+        {/* <mesh position-z={-5} scale={10}>
+          <planeGeometry />
+          <meshBasicMaterial color={[2, 0, 0]} />
+        </mesh> */}
+      </Environment>
+
       {/* <BakeShadows /> */}
       {/* <SoftShadows size={5} samples={10} focus={0} /> */}
       {/* <AccumulativeShadows
@@ -83,22 +140,22 @@ const Experience = () => {
         />
       </AccumulativeShadows> */}
       <ContactShadows
-        position={[0, -0.99, 0]}
+        position={[0, 0, 0]}
         color={contactShadowControls.color}
         opacity={contactShadowControls.opacity}
         blur={contactShadowControls.blur}
         frames={1} //bake
       />
-      <Sky sunPosition={sunPosition} />
+      {/* <Sky sunPosition={sunPosition} /> */}
       <OrbitControls makeDefault />
-      <directionalLight
+      {/* <directionalLight
         ref={directionLightRef}
         castShadow
         position={sunPosition}
         intensity={4.5}
         shadow-mapSize={[1024, 1024]}
-      />
-      <ambientLight intensity={1.5} />
+      /> */}
+      {/* <ambientLight intensity={1.5} /> */}
       {/* <CustomObject /> */}
       <group ref={cubeSphereRef}>
         {/* <PivotControls anchor={[0, 0, 0]} depthTest={false}> */}
@@ -109,7 +166,10 @@ const Experience = () => {
           position={[position.x, position.y, 0]}
         >
           <sphereGeometry />
-          <meshStandardMaterial color={color} />
+          <meshStandardMaterial
+            color={color}
+            envMapIntensity={envMapIntensity}
+          />
           {/* <Html
             position={[1, 1, 0]}
             center
@@ -125,19 +185,26 @@ const Experience = () => {
           ref={cubeRef}
           castShadow
           receiveShadow
+          position-y={1}
           position-x={2}
           rotation-y={Math.PI * 0.25}
           scale={1.5}
         >
           <boxGeometry />
-          <meshStandardMaterial color="mediumpurple" />
+          <meshStandardMaterial
+            color="mediumpurple"
+            envMapIntensity={envMapIntensity}
+          />
         </mesh>
         {/* <TransformControls object={cubeRef} /> */}
       </group>
-      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+      {/* <mesh position-y={0} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
-        <meshStandardMaterial color="greenyellow" />
-      </mesh>
+        <meshStandardMaterial
+          color="greenyellow"
+          envMapIntensity={envMapIntensity}
+        />
+      </mesh> */}
       {/* <Float speed={2} floatIntensity={3}>
         <Text>Sample text</Text>
       </Float> */}
