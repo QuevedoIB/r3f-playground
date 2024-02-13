3195f98a1a7bdae31b97d81e1d772c3761cfc086
Ivan <4esoquevedoi@gmail.com>
2024-02-13 21:53:57 +0100

    feat: 3d text and matcap materials + groups

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,28 +1,84 @@
-import React, { Suspense } from "react";
-import { OrbitControls } from "@react-three/drei";
+import React, { Suspense, useEffect, useRef, useState } from "react";
+import {
+  OrbitControls,
+  Text3D,
+  useMatcapTexture,
+  Center,
+} from "@react-three/drei";
 import { Perf } from "r3f-perf";
+import * as THREE from "three";
+import { useFrame } from "@react-three/fiber";
 
 import Model from "./Model";
 import Hamburger from "./Hamburger";
 import Fox from "./Fox";
 
+const torusGeometry = new THREE.TorusGeometry();
+const material = new THREE.MeshMatcapMaterial();
+
 const Experience = () => {
+  const donutsRef = useRef([]);
+  // const donutsGroupRef = useRef();
+  const [matcapTexture] = useMatcapTexture("7B5254_E9DCC7_B19986_C8AC91", 256); //MATCAP name + size, smaller = better perf
+  const donutsArr = Array(100).fill("");
+  const getRandomPosition = () => (Math.random() - 0.5) * 20;
+  // const [torusRef, setTorusRef] = useState();
+  useEffect(() => {
+    // Set encoding / color space to matcap since material is in native three js
+    matcapTexture.colorSpace = THREE.SRGBColorSpace;
+    matcapTexture.needsUpdate = true;
+
+    // set matcap to material after hook loaded and we have access to it
+    material.matcap = matcapTexture;
+    material.needsUpdate = true;
+  }, []);
+
+  useFrame((state, delta) => {
+    // for (const donut of donutsGroupRef.current.children) {
+    //   donut.rotation.y += delta * 0.5;
+    // }
+  });
+
   return (
     <>
+      {/* <torusGeometry ref={setTorusRef} /> */}
+
       <Perf position="top-left" />
-
       <OrbitControls makeDefault />
-      <directionalLight castShadow position={[1, 2, 3]} intensity={4.5} />
-
-      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
-        <planeGeometry />
-        <meshStandardMaterial color="greenyellow" />
-      </mesh>
-
-      <Suspense>
-        <Hamburger scale={0.35} />
-        <Fox />
-      </Suspense>
+      {/* 
+      <mesh scale={1.5}>
+        <boxGeometry />
+        <meshNormalMaterial />
+      </mesh> */}
+      <Center>
+        <Text3D
+          font="./fonts/helvetiker_regular.typeface.json"
+          material={material}
+        >
+          {/* TEST R3F <meshMatcapMaterial matcap={matcapTexture} /> */}
+          TEXT
+        </Text3D>
+      </Center>
+      {/* <group ref={donutsGroupRef}> */}
+      {donutsArr.map((_, i) => (
+        <mesh
+          ref={(el) => (donutsRef.current[i] = el)}
+          key={i}
+          position={[
+            getRandomPosition(),
+            getRandomPosition(),
+            getRandomPosition(),
+          ]}
+          scale={0.2 + Math.random() * 0.2}
+          rotation={[Math.random() * Math.PI, Math.random() * Math.PI, 0]}
+          // geometry={torusRef}
+          material={material}
+          geometry={torusGeometry}
+        >
+          {/* <meshMatcapMaterial matcap={matcapTexture} /> */}
+        </mesh>
+      ))}
+      {/* </group> */}
     </>
   );
 };
